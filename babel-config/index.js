a67fd20131d8b637b8bc0fbb2cbf0f38776bd32f
Yanko Shterev <yshterev@gmail.com>
2020-06-30 10:29:23 +0300

    core-js, ie11 polyfills - 0.0.16

--- babel-config/index.js
+++ babel-config/index.js
@@ -1,10 +1,21 @@
 module.exports = {
-  presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "entry",
+        corejs: 3,
+        targets: "last 2 versions, > 0.10%, ie 10, not dead"
+      },
+    ],
+    "@babel/preset-react",
+    "@babel/preset-typescript",
+  ],
   plugins: [
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/proposal-class-properties', { loose: true }],
-    '@babel/proposal-object-rest-spread',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/transform-runtime',
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/proposal-class-properties", { loose: true }],
+    "@babel/proposal-object-rest-spread",
+    "@babel/plugin-proposal-optional-chaining",
+    "@babel/transform-runtime",
   ],
 };
