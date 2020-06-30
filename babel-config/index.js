6b7b68ab009c508c573646dbcc7c76e802bfa828
Yanko Shterev <yshterev@gmail.com>
2020-06-30 17:42:32 +0300

    eslint setup for ie11 - 0.0.18

--- babel-config/index.js
+++ babel-config/index.js
@@ -1,21 +1,24 @@
 module.exports = {
   presets: [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
-        useBuiltIns: "entry",
+        useBuiltIns: 'usage',
         corejs: 3,
-        targets: "last 2 versions, > 0.10%, ie 10, not dead"
+        targets: {
+          browsers: ['last 2 versions', 'ie >= 10'],
+        },
       },
     ],
-    "@babel/preset-react",
-    "@babel/preset-typescript",
+    '@babel/preset-react',
+    '@babel/preset-typescript',
   ],
   plugins: [
-    ["@babel/plugin-proposal-decorators", { legacy: true }],
-    ["@babel/proposal-class-properties", { loose: true }],
-    "@babel/proposal-object-rest-spread",
-    "@babel/plugin-proposal-optional-chaining",
-    "@babel/transform-runtime",
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/proposal-class-properties', { loose: true }],
+    '@babel/proposal-object-rest-spread',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/transform-runtime',
   ],
+  sourceType: "unambiguous" // https://github.com/babel/babel/issues/8900#issuecomment-431240426
 };
