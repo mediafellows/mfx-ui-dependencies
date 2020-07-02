072910c05a89bb7568eb6bfe6f992b827802a135
Bartek Cichocki <bartek@codinarium.com>
2020-07-02 10:10:20 +0200

    Unban {} types

--- eslint-config/index.js
+++ eslint-config/index.js
@@ -29,5 +29,13 @@ module.exports = {
     '@typescript-eslint/camelcase': 'off',
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/ban-types": [
+      "error",
+      {
+        "types": {
+          "{}": false
+        }
+      }
+  ]
   },
 };
