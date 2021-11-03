0bc9197317a97e3d08be3f116b4a42f8ab9c3243
Yanko Shterev <yshterev@gmail.com>
2021-11-03 11:25:08 +0200

    Updating packages - minor-1 version

--- babel-config/index.js
+++ babel-config/index.js
@@ -1,24 +1,12 @@
 module.exports = {
   presets: [
-    [
-      '@babel/preset-env',
-      {
-        useBuiltIns: 'usage',
-        corejs: 3,
-        targets: {
-          browsers: ['last 2 versions', 'ie >= 10'],
-        },
-      },
-    ],
-    '@babel/preset-react',
-    '@babel/preset-typescript',
+    "@babel/preset-env",
+    "@babel/preset-react",
+    "@babel/preset-typescript",
   ],
   plugins: [
-    ['@babel/plugin-proposal-decorators', { legacy: true }],
-    ['@babel/proposal-class-properties', { loose: true }],
-    '@babel/proposal-object-rest-spread',
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/transform-runtime',
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    "@babel/plugin-transform-runtime",
   ],
-  sourceType: "unambiguous" // https://github.com/babel/babel/issues/8900#issuecomment-431240426
+  sourceType: "unambiguous", // https://github.com/babel/babel/issues/8900#issuecomment-431240426
 };
